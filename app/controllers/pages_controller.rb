0c435077d1de5438a61701ad5b52eed28a29b5ee
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-05-06 17:27:29 -0400

    :rotating_light: Clean up some rubocop stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
+# A controller to display various static pages
 class PagesController < ApplicationController
   def home
     @posts = Blog.all
   end
 
-  def about
-  end
+  def about; end
 
-  def contact
-  end
+  def contact; end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Helpers for the Pages static pages controller
 module PagesHelper
 end

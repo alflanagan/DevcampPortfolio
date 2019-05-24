72e0bacfe0d250b0fcea161d60a2e17dee5afce7
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-05-24 15:06:20 -0400

    Complete authentication w/virtual attrs for user model (video 94)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,5 @@
 
 class ApplicationController < ActionController::Base
 
-  before_action :configure_permitted, if: :devise_controller?
-
-  def configure_permitted
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+  include DeviseWhitelist
 end

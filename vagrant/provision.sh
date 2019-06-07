dc9d9c956a5769675b09086f8df98b6a5468ac35
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-06-07 16:31:08 -0400

    Install prereqs and clone to Vagrant VM (not part of course)

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -4,22 +4,23 @@
 RVERSION=2.6.3
 NODE_VERSION='lts/*'
 ADMIN_USER=deploy
-SOURCE=https://github.com/alflanagan/DevcampPortfolio.git
-DEST=/home/${ADMIN_USER}/build/DevcampPortfolio
+export ADMIN_USER NODE_VERSION RVERSION
 
-export NODE_VERSION
-
-#### Command 'aliases'
-CURL="command curl"
+#### Commands
+# don't show progress, do show errors
+CURL="command curl -s -S"
 GPG=gpg2
 BUNDLE="bin/bundle"
 # someday CentOS will catch up
 DNF=yum
+export CURL GPG BUNDLE DNF
 
 #### required RPMs
 RUBY_BUILD_DEPS="patch autoconf automake bison gcc-c++ libffi-devel libtool patch readline-devel"
 RUBY_BUILD_DEPS="${RUBY_BUILD_DEPS} ruby sqlite-devel zlib-devel glibc-headers glibc-devel openssl-devel"
+POSTGRES_DEPS="postgresql postgresql-libs postgresql-contrib postgresql-devel postgresql-server"
 OTHER_DEPS="curl git docker"
+export RUBY_BUILD_DEPS POSTGRES_DEPS OTHER_DEPS
 
 #### useful functions
 exit_with_error () {
@@ -30,21 +31,22 @@ exit_with_error () {
 export -f exit_with_error
 
 create_rvm_user () {
-    echo "Creating user $1"
-    groupadd -f rvm
-    grep -q "$1" /etc/passwd || useradd -m -G rvm,wheel -U "$1" || exit_with_error "Failed to create user $1!!" 1
-}
-
-deploy () {
-    runuser ${ADMIN_USER} -c "$1"
-    return $?
+  echo "provision: ${FUNCNAME[0]} entered as ${USER}"
+  echo "Creating user $1"
+  groupadd -f rvm
+  grep -q "$1" /etc/passwd || useradd -m -G rvm,wheel -U "$1" || exit_with_error "Failed to create user $1!!" 1
+  local ERR=$?
+  echo "provision: ${FUNCNAME[0]} completed."
+  return $ERR
 }
 
 install_node () {
-  echo "installing node ${NODE_VERSION} for ${USER}"
-  NVM_DIR=${HOME}/.nvm
+  echo "provision: ${FUNCNAME[0]} entered as ${USER}"
+  local NVM_DIR=${HOME}/.nvm
+  cd
   if [[ ! -d ${NVM_DIR} ]]; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+    echo "provision: ${FUNCNAME[0]}: Downloading nvm"
+    ${CURL} -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
   fi
   # ensure nvm is present
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -53,25 +55,75 @@ install_node () {
     nvm alias default ${NODE_VERSION}
   }
   nvm use ${NODE_VERSION}
+  local ERR=$?
+  echo "provision: ${FUNCNAME[0]} completed."
+  return $ERR
 }
 
-export -f install_node
+install_ruby () {
+  echo "provision: ${FUNCNAME[0]} entered as ${USER}"
+  cd
+  if [[ ! -d ~/.rvm ]]; then
+    ${CURL} -sSL https://rvm.io/mpapis.asc | ${GPG} --import -
+    ${CURL} -sSL https://rvm.io/pkuczynski.asc | ${GPG} --import -
+    ${CURL} -sSL https://get.rvm.io | bash -s stable --rails || exit_with_error 'Unable to install rvm or ruby' 3
+    . ~/.rvm/scripts/rvm
+    rvm install ${RVERSION} || exit_with_error "Unable to install ruby version ${RVERSION}"
+  fi
+  local ERR=$?
+  echo "provision: ${FUNCNAME[0]} completed."
+  return $ERR
+}
+
+clone_repo () {
+  echo "provision: ${FUNCNAME[0]} entered as ${USER}"
+  local SOURCE=https://github.com/alflanagan/DevcampPortfolio.git
+  local DEST=/home/${ADMIN_USER}/build/DevcampPortfolio
+
+  cd
+  . ~/.rvm/scripts/rvm
+  rvm use ${RVERSION}
+  if [[ ! -d "${DEST}" ]]; then
+    mkdir -p ~/build
+    cd ~/build
+    git clone ${SOURCE} || exit_with_error "Failure cloning repository from ${SOURCE}" 7
+  fi
+  cd "${DEST}" || exit_with_error "Directory ${DEST} not created!"
+  git checkout view
+  gem install bundler -v '< 2'
+  ${BUNDLE} install --deployment --without=development
+  local ERR=$?
+  echo "provision: ${FUNCNAME[0]} completed."
+  return $ERR
+}
+
+export -f install_node install_ruby clone_repo
+
+do_as_deploy () {
+  echo "provision: ${FUNCNAME[0]} entered as ${USER}"
+  runuser ${ADMIN_USER} -c install_ruby
+  runuser ${ADMIN_USER} -c install_node
+  runuser ${ADMIN_USER} -c clone_repo
+  local ERR=$?
+  echo "provision: ${FUNCNAME[0]} completed."
+  return $ERR
+}
 
 setup_postgres () {
-  #### required RPMs
-  PSQL_RPMS="postgresql postgresql-libs postgresql-contrib postgresql-devel postgresql-server"
+  echo "provision: ${FUNCNAME[0]} entered as ${USER}"
   #### get RPMS
   # shellcheck disable=SC2086
-  ${DNF} install -y ${PSQL_RPMS} || exit_with_error "${DNF} install of PostgresSQL failed!" 4
+  ${DNF} install -y ${POSTGRES_DEPS} || exit_with_error "${DNF} install of PostgresSQL failed!" 4
 
   #### init data directories
   runuser postgres -c "postgresql-setup initdb"
 
+  #### start it
   systemctl enable postgresql
-
   systemctl start postgresql
-
-  echo "************ Completed $(basename $0) ******************"
+  local ERR=$?
+  echo "provision: ${FUNCNAME[0]} completed."
+  return $ERR
 }
 
 #### get RPMS
@@ -85,40 +137,4 @@ setup_postgres
 #### user-side setup
 create_rvm_user ${ADMIN_USER}
 
-ruby_script=$(cat <<RUBY
-  cd
-  if [[ ! -d ~/.rvm ]]; then
-    ${CURL} -sSL https://rvm.io/mpapis.asc | ${GPG} --import -
-    ${CURL} -sSL https://rvm.io/pkuczynski.asc | ${GPG} --import -
-    ${CURL} -sSL https://get.rvm.io | bash -s --help --rails || exit_with_error 'Unable to install rvm or ruby' 3
-    rvm install ${RVERSION}
-  fi
-RUBY
-)
-
-deploy "${ruby_script}"
-
-deploy install_node
-
-echo "ruby_script completed"
-
-git_script=$(cat <<-GIT
-  . ~/.rvm/scripts/rvm
-  rvm use ${RVERSION}
-  if [[ ! -d "${DEST}" ]]; then
-    echo "${DEST} not found"
-    ls -A build
-    mkdir -p ~/build
-    cd ~/build
-    git clone ${SOURCE} || exit_with_error "Failure cloning repository from ${SOURCE}" 7
-  fi
-  cd "${DEST}" || exit_with_error "Directory ${DEST} not created!"
-  git checkout view
-  gem install bundler -v '< 2'
-  ${BUNDLE} install --deployment --without=development
-GIT
-)
-
-deploy "${git_script}"
-
-echo "git_script completed" >&2
+do_as_deploy

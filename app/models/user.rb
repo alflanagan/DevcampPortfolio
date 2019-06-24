8c48d8aeae9d2947e49f3dc0d8b84cbe94e2395d
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-06-24 13:19:28 -0400

    Add installation requirements for petergate (video 131)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
+# An authenticated user. Enables permissions and roles.
 class User < ApplicationRecord
+  ############################################################################################
+  ## PeterGate Roles                                                                        ##
+  ## The :user role is added by default and shouldn't be included in this list.             ##
+  ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
+  ## The multiple option can be set to true if you need users to have multiple roles.       ##
+  petergate(roles: [:admin, :editor], multiple: false)
+  ############################################################################################
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,10 +18,10 @@ class User < ApplicationRecord
   validates_presence_of :name
 
   def first_name
-    self.name.split.first
+    @name.split.first
   end
 
   def last_name
-    self.name.split.rest
+    @name.split.rest
   end
 end

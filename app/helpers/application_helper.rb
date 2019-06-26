449143b8f7948318539e1161173683010263f0c2
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-06-26 16:16:09 -0400

    Change default class to empty string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@
 # application-wide helper methods
 module ApplicationHelper
   # return a link to 'Logout', or two to 'Register' and 'Login', as appropriate for current user
-  def login_helper(style = 'nav-link')
+  def login_helper(style = '')
     if current_user.is_a? GuestUser
       (link_to 'Register', new_user_registration_path, class: style) +
         (link_to 'Login', new_user_session_path, class: style)

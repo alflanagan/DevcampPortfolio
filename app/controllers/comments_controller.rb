ccc5b2c46ebf72d5dff120b1248deccb4025690c
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-07-15 15:26:30 -0400

    Add form and partial for comments (video 190)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,13 @@
 
 # Actions related to user comments on blog entries.
 class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.build(comment_params)
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end

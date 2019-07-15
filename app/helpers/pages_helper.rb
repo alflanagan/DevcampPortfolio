74941d234e9d460a908f706e9fb1eff5d454bfca
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-07-15 13:47:45 -0400

    Implement loading of lib directory (video 186)

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,21 @@
 
 # Helpers for the Pages static pages controller
 module PagesHelper
+  URI_REGEX = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix.freeze
 end

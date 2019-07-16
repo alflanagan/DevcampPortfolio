489b2339a0c7b74791bd162842a66859e1060257
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-07-16 12:22:02 -0400

    Complete comment feature with actioncable (video 197)

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,7 @@
 
 # Helpers for the Blogs controller
 module BlogsHelper
+  def gravatar_helper user
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+  end
 end

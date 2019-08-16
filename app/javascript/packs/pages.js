220d88d122cba2a4626e905e4f54f8ff98b9410f
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-08-16 16:01:09 -0400

    Add standardjs, update pages.js to match

--- app/javascript/packs/pages.js
+++ app/javascript/packs/pages.js
@@ -1,26 +1,26 @@
+/* global $ */
 
 export class Pages {
   // keep track of row being edited
-  static CURRENT_EDIT_ROW = -1  // -1 ==> no row edited
+  static CURRENT_EDIT_ROW = -1 // -1 ==> no row edited
 
-  static trigger_skill_edit (e) {
-
-    if (Pages.CURRENT_EDIT_ROW == -1) {
+  static triggerSkillEdit (e) {
+    const skillId = e.target.dataset['skillId']
+    if (Pages.CURRENT_EDIT_ROW === -1) {
       Pages.CURRENT_EDIT_ROW = skillId
     } else {
       return // we're already editing a row!!
     }
 
-    const skillId = e.target.dataset['skillId']
-    const row_element = $(e.target.parentElement)
+    const rowElement = $(e.target.parentElement)
 
-    row_element.css({display: 'none'})
+    rowElement.css({ display: 'none' })
 
-    const edit_skill_id = `#edit_skill_${skillId}`
-    $(edit_skill_id).css({display: 'block'})
+    const editSkillId = `#edit_skill_${skillId}`
+    $(editSkillId).css({ display: 'block' })
   }
 
-  static submit_skill_edit () {
+  static submitSkillEdit () {
 
   }
 }

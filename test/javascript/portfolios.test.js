e124e40ee8f62cfb50fa0f5431a4a43d33e32b06
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-08-21 16:21:50 -0400

    :bug: Fix jest tests to properly import jquery.

--- test/javascript/portfolios.test.js
+++ test/javascript/portfolios.test.js
@@ -1,9 +1,11 @@
 'use strict'
 
+import $ from 'jquery'
 import {
   Portfolio
 } from '../../app/javascript/packs/portfolios'
-import $ from 'jquery'
+
+global.$ = global.jQuery = $
 
 test('setPositions updates 1 DOM element with "card" class', () => {
   document.body.innerHTML = '<div><div class="card"><p>Some text.</p></div></div>'
